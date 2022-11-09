5a3787b9cbf25e5445943ceb50910f99024440ff
chiisen <chiisen@hotmail.com>
2022-11-09 17:16:24 +0800

    feat: 新增面額與比率轉換

--- src/data.js
+++ src/data.js
@@ -72,4 +72,78 @@ function denomIndexToDenomString(denom) {
   throw msg_
 }
 
-module.exports = { denomStringToDenomIndex, denomIndexToDenomString }
+/**
+ * 面額轉成比率
+ * @note 先不改名稱，升級時才能比對程式碼
+ */
+const denomToRatioMap = new Map([
+  ["1:100000", 0.00001],
+  ["1:50000", 0.00002],
+  ["1:10000", 0.0001],
+  ["1:5000", 0.0002],
+  ["1:2000", 0.0005],
+  ["1:1000", 0.001],
+  ["1:500", 0.002],
+  ["1:200", 0.005],
+  ["1:100", 0.01],
+  ["1:50", 0.02],
+  ["1:20", 0.05],
+  ["1:10", 0.1],
+  ["1:5", 0.2],
+  ["1:2", 0.5],
+  ["1:1", 1],
+  ["2:1", 2],
+  ["5:1", 5],
+  ["10:1", 10],
+  ["20:1", 20],
+  ["50:1", 50],
+  ["100:1", 100],
+  ["200:1", 200],
+  ["500:1", 500],
+  ["1000:1", 1000],
+  ["2000:1", 2000],
+  ["5000:1", 5000],
+  ["10000:1", 10000],
+  ["50000:1", 50000],
+  ["100000:1", 100000],
+])
+
+/**
+ * 面額【字串】轉面額【比率】
+ *
+ * @param {*} denom
+ * @returns
+ */
+function denomStringToDenomRatio(denom) {
+  if (typeof denom === "string" || denom instanceof String) {
+    return denomToRatioMap.get(denom)
+  }
+  const msg_ = `denom: '${denom}' not string`
+  console.error(msg_)
+  throw msg_
+}
+
+/**
+ * 面額【比率】轉成面額【字串】
+ * @note 先不改名稱，升級時才能比對程式碼
+ */
+const ratioToDenomArrayMap = swapMap(denomToRatioMap)
+
+/**
+ * 面額【比率】轉成面額【字串】
+ *
+ * @param {*} denom
+ *
+ */
+function denomRatioToDenomString(denom) {
+  if (isNumber(denom)) {
+    //@note 使用 swapMap 轉換後，都會變成陣列，目前還沒辦法調整為字串，之後會再調整
+    const denomString_ = ratioToDenomArrayMap.get(denom)
+    return denomString_[0]
+  }
+  const msg_ = `denom: '${denom}' not number`
+  console.error(msg_)
+  throw msg_
+}
+
+module.exports = { denomStringToDenomIndex, denomIndexToDenomString, denomStringToDenomRatio, denomRatioToDenomString }
